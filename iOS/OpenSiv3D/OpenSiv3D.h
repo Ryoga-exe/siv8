2abf0572b27cb953b27cf560c210a63b1987da31
Ryoga-exe <git@ryoga.dev>
2025-11-11 02:04:40 +0900

    feat: add iOS project

---
+++ iOS/OpenSiv3D/OpenSiv3D.h
@@ -0,0 +1,10 @@
+//
+//  OpenSiv3D.h
+//  OpenSiv3D
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenSiv3D : NSObject
+
+@end
---
+++ iOS/OpenSiv3D/OpenSiv3D.m
@@ -0,0 +1,10 @@
+//
+//  OpenSiv3D.m
+//  OpenSiv3D
+//
+
+#import "OpenSiv3D.h"
+
+@implementation OpenSiv3D
+
+@end
